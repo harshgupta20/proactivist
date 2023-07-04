d38945762d557b9806f99546eff8489869a03f8a
Harsh Gupta <59175412+harshgupta20@users.noreply.github.com>
2023-07-05 01:22:23 +0530

    push to hub

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,16 +2,26 @@ import React from 'react'
 import "../styles/Navbar.css";
 
 const Navbar = () => {
+
+  const handleClickScroll = () => {
+    const element = document.getElementById('featured-main');
+    if (element) {
+      // 👇 Will scroll smoothly to the top of the next section
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+
+  }
+
   return (
     <>
         <nav id="nav">
             <h1>Proactivist</h1>
 
             <ul id="nav-ul">
-                <a href="#" id="nav-a"><li id="nav-li">Blogs</li></a>
-                <a href="#" id="nav-a"><li id="nav-li">Resources</li></a>
-                <a href="#" id="nav-a"><li id="nav-li">Team</li></a>
-                <a href="#" id="nav-a"><li id="nav-li"><button id="nav-btn">Join</button></li></a>
+                <li onClick={handleClickScroll} id="nav-li">Blogs</li>
+                <li id="nav-li">Resources</li>
+                <li id="nav-li">Team</li>
+                <li id="nav-li"><button id="nav-btn">Join</button></li>
 
             </ul>
         </nav>
---
+++ src/sections/Blogs.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Blogs = () => {
+  return (
+    <>
+        <div id="blog-main">
+            
+        </div>
+    </>
+  )
+}
+
+export default Blogs

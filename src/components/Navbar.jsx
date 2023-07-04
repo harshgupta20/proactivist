69e93193b9b0d4c04363831ec0a3a1b57653cb11
Harsh Gupta <59175412+harshgupta20@users.noreply.github.com>
2023-07-05 00:04:52 +0530

    navbar added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import "../styles/Navbar.css";
+
+const Navbar = () => {
+  return (
+    <>
+        <nav id="nav">
+            <h1>Proactivist</h1>
+
+            <ul id="nav-ul">
+                <a href="" id="nav-a"><li id="nav-li">Blogs</li></a>
+                <a href="" id="nav-a"><li id="nav-li">Resources</li></a>
+                <a href="" id="nav-a"><li id="nav-li">Team</li></a>
+                <a href="" id="nav-a"><li id="nav-li"><button id="nav-btn">Join</button></li></a>
+
+            </ul>
+        </nav>
+    </>
+  )
+}
+
+export default Navbar
